2ca0662683306c15f5a53b2ed27dc56feba63914
Himanshu <kumarhimanshu2219@gmail.com>
2023-03-29 18:52:41 +0530

    Home Page Design Completed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,24 +1,25 @@
 import { IoMdMic, IoMdSearch } from "react-icons/io";
 import { BiCamera } from "react-icons/bi";
+import logoMic from "../assets/logo-mic.svg";
+import logoCamera from "../assets/logo-camera.svg";
 
 export const SearchBar = ({ placeholder, styleClass }) => {
   return (
     <>
       <div className="relative ">
-        <input type="text" className={styleClass} placeholder={placeholder} />
+        <input
+          type="text"
+          className={styleClass}
+          placeholder={placeholder}
+          autoFocus
+        />
         <div>
-          <IoMdMic
-            className="absolute right-14 top-3 text-[#8e9499]"
-            size={25}
-          />
-          <BiCamera
-            className="absolute right-3 top-3 text-[#8e9499] "
-            size={25}
-          />
           <IoMdSearch
             className="absolute left-3 top-4 text-[#8e9499]"
             size={20}
           />
+          <img src={logoMic} className="absolute right-14 top-3 h-7" />
+          <img src={logoCamera} className="absolute right-3 top-3 h-6" />
         </div>
       </div>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,52 @@
 import { Image } from "../components/Image";
 import googleLogo from "../assets/logo-google.png";
 import { SearchBar } from "../components/SearchBar";
+import { Button } from "../components/Button";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col justify-center items-center ">
-        <Image image={googleLogo} styleClass={"h-24 mt-2 text-center mb-6"} />
+        <Image
+          image={googleLogo}
+          styleClass={"h-[90px] mt-3 text-center mb-6"}
+        />
         <SearchBar
           styleClass={
-            "rounded-full w-[35rem] pb-3 pt-3 pl-10 pr-10 border focus:outline-none hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
+            "rounded-full w-[37rem] pb-3 pt-3 pl-10 pr-10 border focus:outline-none  focus-within:shadow-[0_3px_10px_rgb(0,0,0,0.2)] focus-within:border-0"
           }
         />
+        <div className="flex space-x-3 space-y-8">
+          <div></div>
+          <Button
+            title={"Google Search"}
+            styleClass={
+              "pt-2 pb-2 pr-4 pl-4 bg-[#F0F1F2] text-black font-light rounded text-white text-sm"
+            }
+          />
+          <Button
+            title={"I'm Feeling Lucky"}
+            styleClass={
+              "pt-2 pb-2 pr-4 pl-4 bg-[#F0F1F2] text-black font-light rounded text-white text-sm"
+            }
+          />
+        </div>
+        <div className="flex items-center text-[#1a0dab]  space-x-2  text-xs mt-8">
+          <div>
+            <p className="text-black text-[13px] ">Google offered in : </p>
+          </div>
+          <Link>हिन्दी</Link>
+          <Link>বাংলা</Link>
+          <Link>తెలుగు</Link>
+          <Link>मराठी</Link>
+          <Link>தமிழ்</Link>
+          <Link>தமிழ்</Link>
+          <Link>ગુજરાતી</Link>
+          <Link>ಕನ್ನಡ</Link>
+          <Link>മലയാളം</Link>
+          <Link>ਪੰਜਾਬੀ</Link>
+        </div>
       </div>
     </>
   );
