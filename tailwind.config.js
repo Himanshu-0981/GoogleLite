3e5b842bd520f0712e54b65d02ee98b788b408e1
Himanshu <kumarhimanshu2219@gmail.com>
2023-03-29 12:26:49 +0530

    Tailwind configured

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,jsx,js}",
+    "./src/**/*.{html,jsx,js}",
+    "./components/**/*.{html,jsx,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
