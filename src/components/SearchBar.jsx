9b74fc2253bb4ad6daa78be35c89cad2a9c9c63a
Himanshu <kumarhimanshu2219@gmail.com>
2023-03-29 16:28:25 +0530

    Search Section Completed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,25 @@
-export const SearchBar = ({}) => {
+import { IoMdMic, IoMdSearch } from "react-icons/io";
+import { BiCamera } from "react-icons/bi";
+
+export const SearchBar = ({ placeholder, styleClass }) => {
   return (
     <>
-      <div>
-        <input type="text" />
+      <div className="relative ">
+        <input type="text" className={styleClass} placeholder={placeholder} />
+        <div>
+          <IoMdMic
+            className="absolute right-14 top-3 text-[#8e9499]"
+            size={25}
+          />
+          <BiCamera
+            className="absolute right-3 top-3 text-[#8e9499] "
+            size={25}
+          />
+          <IoMdSearch
+            className="absolute left-3 top-4 text-[#8e9499]"
+            size={20}
+          />
+        </div>
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import { Image } from "../components/Image";
 import googleLogo from "../assets/logo-google.png";
+import { SearchBar } from "../components/SearchBar";
 
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center">
-        <Image image={googleLogo} styleClass={"h-24 mt-2 text-center"} />
+      <div className="flex flex-col justify-center items-center ">
+        <Image image={googleLogo} styleClass={"h-24 mt-2 text-center mb-6"} />
+        <SearchBar
+          styleClass={
+            "rounded-full w-[35rem] pb-3 pt-3 pl-10 pr-10 border focus:outline-none hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
+          }
+        />
       </div>
     </>
   );
